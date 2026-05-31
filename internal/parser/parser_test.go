f0dce19d92f1ed1f7ed9c1f93234933d2499b15f
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2026-05-31 22:17:53 +0100

    starting tests

---
+++ internal/parser/parser_test.go
@@ -0,0 +1,38 @@
+package parser
+
+import (
+	"testing"
+
+	"github.com/rpstvs/json-parser-go/internal/ast"
+	"github.com/rpstvs/json-parser-go/internal/lexer"
+)
+
+func TestJSONObjectChildren(t *testing.T) {
+	tests := []struct {
+		input       string
+		childrenLen int
+	}{
+		{input: "{\"key0\": \"value0\"}", childrenLen: 1},
+		{input: "{\"key0\": \"value0\" }", childrenLen: 1},
+		{input: "{\"key1\": \"value1\", \"key2\": \"value2\"}", childrenLen: 2},
+		{input: "{\"key3\": [\"value3\", \"value4\"]}", childrenLen: 1},
+		{input: "{\"key4\": [\"value5\", {\"key5\": \"value6\"}]}", childrenLen: 1},
+		{input: "{\"key5\":\" value7\", \"key6\": \"value7\"}", childrenLen: 2},
+		{input: "{\"key5\":\" value7\", \"key6\": \"value7\", \"key7\": \"value8\"}", childrenLen: 3},
+	}
+
+	for _, test := range tests {
+		l := lexer.New(test.input)
+		p := New(l)
+
+		program := p.ParseJSON()
+
+		rv := program.RootValue
+
+		val := rv.Value.(ast.Object)
+
+		if len(val.Children) != test.childrenLen {
+			t.Fatalf("The length of the children does not contain 1 statement. Got: %d", len(val.Children))
+		}
+	}
+}
